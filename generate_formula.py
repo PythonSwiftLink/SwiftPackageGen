a79de423f2210ed3c964a170bc166b277036a5af
psychowasp <psychowaspx@gmail.com>
2023-10-18 13:24:26 +0200

    formula generator

---
+++ generate_formula.py
@@ -0,0 +1,71 @@
+#from cookiecutter.main import cookiecutter
+from os.path import join, abspath
+from os import pardir, getcwd
+import sys
+
+if not len(sys.argv) >= 3: 
+    exit(-1)
+
+version = sys.argv[1]
+sha = sys.argv[2]
+
+
+
+template0 = """
+class Swiftpackagegen < Formula
+  desc ""
+  homepage ""
+
+  url "https://github.com/PythonSwiftLink/SwiftPackageGen/releases/download/{version}/SwiftPackageGen.tar.gz"
+  version "{version}"
+  sha256 "{sha}"
+  license ""
+
+  def install
+    bin.install "SwiftPackageGen"
+    end
+
+  test do
+    system "false"
+  end
+end
+"""
+
+template0 = """
+class Swiftpackagegen{version_name} < Formula
+  depends_on "xcodegen"
+  desc ""
+  homepage ""
+
+  url "https://github.com/PythonSwiftLink/SwiftPackageGen/releases/download/{version}/SwiftPackageGen.tar.gz"
+  version "{version}"
+  sha256 "{sha}"
+  license ""
+
+  def install
+    bin.install "SwiftPackageGen"
+    end
+
+  test do
+    system "false"
+  end
+end
+"""
+
+# template_dir = "https://github.com/PythonSwiftLink/BrewFormulaCookie.git"
+
+# context = {
+#     "version": version,
+#     "name_version": version.replace(".",""),
+#     "sha": sha
+# }
+
+formula_lastest = template0.format(version=version, version_name="", sha=sha)
+
+formula_version = template0.format(version=version, version_name=f'AT{version.replace(".","")}', sha=sha)
+
+with open("homebrew_formula/SwiftPackageGen.rb", "w") as f:
+    f.write(formula_lastest)
+
+with open(f"homebrew_formula/SwiftPackageGen@{version}.rb", "w") as f:
+    f.write(formula_version)
